f6072148bf5a286b1e0a6c809793efe610a4248e
daxchx <daxchx.v1@gmail.com>
2024-01-27 17:18:44 +0900

    Add comment out

--- src/model/deck.ts
+++ src/model/deck.ts
@@ -1,54 +1,55 @@
-import Card from "./card";
+import Card from './card'
 
 export default class Deck {
-  public gameType: string;
-  public cards: Card[];
+  public gameType: string
+  public cards: Card[]
 
   constructor(gameType: string) {
-    this.gameType = gameType;
-    this.cards = [];
+    this.gameType = gameType
+    this.cards = []
 
-    if (this.gameType === "blackjack") {
-      const suits = ["H", "D", "C", "S"];
-      const values = [
-        "A",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "10",
-        "J",
-        "Q",
-        "K",
-      ];
+    if (this.gameType === 'blackjack') {
+      const suits = ['H', 'D', 'C', 'S']
+      const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
       for (let i = 0; i < suits.length; i++) {
         for (let j = 0; j < values.length; j++) {
-          this.cards.push(new Card(suits[i], values[j]));
+          this.cards.push(new Card(suits[i], values[j]))
         }
       }
     }
   }
 
+  /**
+   * デッキをシャッフルする
+   *
+   * @returns {void}
+   */
   public shuffle(): void {
     for (let i = this.cards.length - 1; i >= 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      let temp = this.cards[i];
-      this.cards[i] = this.cards[j];
-      this.cards[j] = temp;
+      let j = Math.floor(Math.random() * (i + 1))
+      let temp = this.cards[i]
+      this.cards[i] = this.cards[j]
+      this.cards[j] = temp
     }
   }
 
+  /**
+   * デッキをリセットする
+   *
+   * @returns {void}
+   */
   public resetDeck(): void {
-    let newCards = new Deck(this.gameType);
-    this.cards = newCards.cards;
+    let newCards = new Deck(this.gameType)
+    this.cards = newCards.cards
   }
 
+  /**
+   * デッキからカードを1枚引く
+   *
+   * @returns {Card}
+   */
   public drawOne(): Card {
-    return this.cards.pop() as Card;
+    return this.cards.pop() as Card
   }
 }
