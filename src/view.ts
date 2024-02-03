88badb2fbe34890bd37e375e417d320264e4ae2b
daxchx <daxchx.v1@gmail.com>
2024-02-03 17:43:24 +0900

    Add style to game start button

--- src/view.ts
+++ src/view.ts
@@ -38,7 +38,9 @@ export default class View {
           <option value="10">10</option>
         </select>
       </div>
-      <button id="create" class="text-sm bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto">はじめる</button>
+      <div class="mt-4 mx-6">
+        <button id="create" class="text-sm bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto">はじめる</button>
+      </div>
     </div>
     `
   }
